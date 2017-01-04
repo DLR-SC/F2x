057298db1fc10e3224e62d540914def22cb968ec
led02 <michael.meinel@dlr.de>
2017-01-04 13:29:19 +0100

    Fixed setup.py to include path to custom setup commands at the beginning of the PYTHONPATH to allow re-installation of new version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
 import setuptools
 from distutils import core
