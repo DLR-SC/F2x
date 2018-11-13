1175221c708349a2788dc3dd045f5be1a4b399d4
Michael Meinel <michael.meinel@dlr.de>
2018-11-13 14:48:34 +0100

    Added missing license headers.

--- src/F2x/distutils/strategy/extension.py
+++ src/F2x/distutils/strategy/extension.py
@@ -1,3 +1,18 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2018 German Aerospace Center (DLR)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 import re
 
@@ -12,11 +27,6 @@ ext_matches = cxx_ext_match, fortran_ext_match, c_ext_match
 
 
 class ExtensionBuildStrategy(BuildStrategy):
-    def __init__(self, templates=None):
-        super(ExtensionBuildStrategy, self).__init__(templates)
-
-        self.is_splitting = False
-
     def finish_extension(self, extension):
         super(ExtensionBuildStrategy, self).finish_extension(extension)
 
